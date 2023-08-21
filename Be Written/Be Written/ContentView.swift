631c034b0760efe02ac1eb128998ea9ac5f8ae0c
wd-ethan <ethawright@gmail.com>
2023-08-20 19:03:58 -0700

    Added map, factored out QuoteView

--- Be Written/Be Written/ContentView.swift
+++ Be Written/Be Written/ContentView.swift
@@ -6,16 +6,22 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    // This is fake data. If you couldn't tell.
     let tileData = ["Today", "Yesterday", "The day before", "The day before that", "... you guess it."]
+    let quote = "Insert Knowledge here, perhaps why there's still white backgrounds."
+    let locationCoordinate = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
     
+    // Real code follows.
     var body: some View {
         NavigationView {
             ScrollView {
+                QuoteView(title: "Quote of the day", quote: quote)
                 LazyVGrid(columns: [GridItem(.flexible())]) {
                     ForEach(tileData, id: \.self) { data in
-                        CardView(title: data, content: AnyView(TileView(data: data)))
+                        CardView(title: data, content: AnyView(TileView(coordinate: locationCoordinate, data: data)))
                     }
                 }
             }
--- Be Written/Be Written/Note.swift
+++ Be Written/Be Written/Note.swift
@@ -7,46 +7,81 @@
 
 import Foundation
 import SwiftUI
+import MapKit
 
-class Note: Identifiable, ObservableObject {
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
     
-    let id = UUID()
+    @State private var mapImage: UIImage? = nil
+        
+    var body: some View {
+        Image(uiImage: mapImage ?? UIImage())
+            .resizable()
+            .scaledToFit()
+            .onAppear {
+                generateMapImage()
+            }
+    }
+    
+    private func generateMapImage() {
+        let options = MKMapSnapshotter.Options()
+        options.region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        options.size = CGSize(width: 300, height: 300)
+        options.mapType = .standard
+        
+        let snapshotter = MKMapSnapshotter(options: options)
+        
+        snapshotter.start { snapshot, error in
+            guard let snapshot = snapshot, error == nil else {
+                return
+            }
+            
+            mapImage = snapshot.image
+        }
+    }
+}
+
+struct QuoteView: View {
     var title: String
-    var date: Date
-    var content: String
+    var quote: String
     
-    init() {
-        self.date = Date()
-        self.title = self.date.formatted()
-        self.content = ""
+    var body: some View {
+        CardView(title: title, content: AnyView(Text(quote)))
     }
 }
 
 struct TileView: View {
+    var coordinate: CLLocationCoordinate2D
     var data: String
     
     var body: some View {
         HStack {
-            Text(data)
+            Text("Insert daily writing here")
                 .font(.headline)
                 .padding()
                 .background(Color.blue)
                 .foregroundColor(.white)
                 .cornerRadius(10)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+            
+            Spacer()
+            
             VStack{
-                Text("HI")
+                Text("Some kinda information")
                     .font(.headline)
                     .padding()
                     .background(Color.blue)
                     .foregroundColor(.white)
                     .cornerRadius(10)
-                Text("bye")
+                
+                MapView(coordinate: coordinate)
                     .font(.headline)
                     .padding()
                     .background(Color.blue)
                     .foregroundColor(.white)
                     .cornerRadius(10)
             }
+            .frame(maxWidth: .infinity, alignment: .trailing)
         }
         .padding()
     }
@@ -71,7 +106,7 @@ struct CardView: View {
                 Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.gray.opacity(0.2))
+            .background(.gray)
             .cornerRadius(20)
             .padding()
         }
